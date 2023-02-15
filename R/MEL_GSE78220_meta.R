96d2e6e8c80d626199df62beb61fe5010341d546
Chengxugorilla <1917720764@qq.com>
2023-02-15 22:12:37 +0800

    renew

--- R/MEL_GSE78220_meta.R
+++ R/MEL_GSE78220_meta.R
@@ -14,34 +14,4 @@ NULL
 #' @source public data set
 NULL
 
-#' @title MEL_GSE91061_exp
-#' @description an expression matrix
-#' @name MEL_GSE91061_exp
-#' @docType data
-#' @format matrix
-#' @source public data set
-NULL
 
-#' @title MEL_GSE91061_meta
-#' @description meta info
-#' @name MEL_GSE91061_meta
-#' @docType data
-#' @format dataframe
-#' @source public data set
-NULL
-
-#' @title MEL_phs000452_exp
-#' @description an expression matrix
-#' @name MEL_phs000452_exp
-#' @docType data
-#' @format matrix
-#' @source public data set
-NULL
-
-#' @title MEL_phs000452_meta
-#' @description meta info
-#' @name MEL_phs000452_meta
-#' @docType data
-#' @format dataframe
-#' @source public data set
-NULL
