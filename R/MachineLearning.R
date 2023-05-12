a26ca6653616ed864b3dcff46a4f0ff8f0c62983
Chengxugorilla <1917720764@qq.com>
2023-05-12 22:47:01 +0800

    fix bug

--- R/MachineLearning.R
+++ R/MachineLearning.R
@@ -92,7 +92,7 @@ build_SVM_model <- function(SE, Signature, rmBE = TRUE, response_NR){
   model <- e1071::svm(x = t(na.omit(exp_mtr)),
                       y = as.factor(meta$response),
                       scale = TRUE,
-                      type = 'C',
+                      type = 'eps',
                       kernel = 'radial',
                       probability = TRUE)
   return(model)
